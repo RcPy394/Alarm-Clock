9a3f2d822bf84b70e97fdad38858b200efa9c3a1
RcPy394 <77559404+RcPy394@users.noreply.github.com>
2021-11-11 13:24:28 -0500

    Add files via upload

---
+++ AlarmClock/Clock.py
@@ -0,0 +1,51 @@
+import time as tm
+import os
+import tkinter as tk
+from tkinter import *
+from pygame import mixer
+mixer.init()
+def getfromfile(rootdir,e):
+    filelist = []
+    counter = 1
+    for root, directories, filenames in os.walk(rootdir):
+        for filename in filenames:
+            if any(ext in filename for ext in e):
+                filelist.append(os.path.join(root, filename))
+                counter += 1
+    return filelist
+def Stop():
+    mixer.music.stop()
+    sleep['state']=DISABLED
+def settune(clicked):
+    mixer.music.load(clicked)
+def PlayMusic():
+    mixer.music.play(-1)
+    sleep['state'] = NORMAL
+def AlarmClock():
+    clock = tk.Label(window, width=5, font=('OCR A Extended', 80, 'bold'),bg="white", fg='blue')
+    clock.place(x=80,y=50)
+    currentTime = tm.strftime('%H:%M:%S')
+    ct = tm.strftime('%H:%M')
+    clock['text'] = ct
+    if (currentTime == inputtxt.get()+':00'):
+        PlayMusic()
+    window.after(1000,AlarmClock)
+window = tk.Tk()
+Music=getfromfile('music','mp3')
+mixer.music.load(Music[0])
+clicked = StringVar()
+clicked.set(Music[0])
+window.config(bg="navy")
+window.geometry("500x250")
+window.title('Alarm Clock')
+sleep = tk.Button(window, text="Stop Alarm", width=40,state=DISABLED, bg='white', command=Stop)
+sleep.place(x=95, y=180)
+inputtxt = tk.Entry(window)
+inputtxt.place(x=175,y=30)
+drop = OptionMenu(window,clicked,*Music,command=settune)
+drop.place(x=80,y=210)
+AlarmClock()
+window.mainloop()
+
+
+
